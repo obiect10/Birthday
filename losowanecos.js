891ebac970be88c9dc214ae63d18d9bdcb4c8bc8
obiect10 <124463019+obiect10@users.noreply.github.com>
2023-04-11 00:56:24 +0200

    1ver

---
+++ losowanecos.js
@@ -0,0 +1,93 @@
+let x = Math.round(Math.random() * 30) + 1;
+if (x === 1) {
+    document.write("----------------Jeszcze to czytasz?----------------");
+};
+if (x === 2) {
+    document.write("🐲🐝👣ↂ🌓ℳ");
+    alert("Zhakowany!? Szpiegowski rząd..");
+};
+if (x === 3) {
+    document.write("Did you washed your ass today??");
+};
+if (x === 4) {
+    document.write("Sie kämpfen für die Freiheit der Welt, sie kämpfen für die Demokratie, sie kämpfen für die Kultur, sie kämpfen für die Zivilisation, sie kämpfen für die Gerechtigkeit");
+};
+if (x === 5) {
+    document.write(">:(");
+    alert("!!!!!!!!!!!!!!");
+    alert("!!!!WTF????!!!!");
+    alert("Hej Pięknisiu >:(");
+};
+if (x === 6) {
+    document.write("⛤⛧⛥⸸⛤⛧⛥⸸Nie, to kacper :)⛤⛧⛥⸸⛤⛧⛥⸸");
+    alert("🔥 Diaboł? ☠⚰️")
+};
+if (x === 7) {
+    document.write(";)");
+};
+if (x === 8) {
+    document.write("Moan, ohh mommyy, gimme milk!!!");
+};
+if (x === 9) {
+    document.write("Ayyyy, ohhh daddyy, yes, you are back!!!!");
+};
+if (x === 10) {
+    document.write("Not for Gordon Freeman 🤫");
+};
+if (x === 11) {
+    document.write("Ur Sussy Baka");
+};
+if (x === 12) {
+    document.write("1.8 log: H.E.R.O.B.R.I.N.E was removed");
+};
+if (x === 13) {
+    document.write("Odgłosy seksu żółwi to twoje nowe motto?");
+};
+if (x === 14) {
+    document.write("Poród hieny jest cholernie bolesny");
+};
+if (x === 15) {
+    document.write("Bóg nie istnieje, change my mind ☕");
+};
+if (x === 16) {
+    document.write("Women ☕");
+};
+if (x === 17) {
+    document.write("1010011 1110000 1110010 1111001 1100011 1101001 1110101 1101100 1100001");
+};
+if (x === 18) {
+    document.write("https://fusionanomaly.net/");
+};
+if (x === 19) {
+    document.write("Secrets: https://youtu.be/M0htjgLCaqM , https://youtu.be/1zXpWmox-DA , https://youtu.be/FNOk6XrvBcg");
+};
+if (x === 20) {
+    document.write("We are going to hiperboea, jack...: https://youtu.be/1e22Zb04z0A");
+};
+if (x === 21) {
+    document.write("You live in a delusion...");
+};
+if (x === 23) {
+    document.write("??👽 https://obscurban-legend.fandom.com/wiki/4chan_Grey_Alien , https://archive.4plebs.org/x/thread/21973079/ , https://deoxy.org/2013.html 🪬??");
+};
+if (x === 24) {
+    document.write("Drzewa komunikują się ze sobą dzięki grzybom 🤓");
+};
+if (x === 25) {
+    document.write("~~~~Nostalgia~~~~~ https://youtu.be/AHZ3cOm-F4M , https://youtu.be/fv0wWVR5TEU , https://youtu.be/0umRw0nP9P0");
+};
+if (x === 26) {
+    document.write("Music but... see by yourself https://youtu.be/BU--dwFW6ic , https://youtu.be/PxTMrrb1BbM , https://youtu.be/sygWZeDYN3o");
+};
+if (x === 27) {
+    document.write("Music!! https://youtu.be/A4d_sLSPWzE , https://youtu.be/J_DV65rmXbM , XD https://youtu.be/I3T_JotUsMk");
+};
+if (x === 28) {
+    document.write("This site is SCP!?? http://scp-pl.wikidot.com/ , https://scp-wiki.wikidot.com/");
+};
+if (x === 29) {
+    document.write("Ktoś, żyje.. w tobie? https://youtube.com/shorts/V8FZHdfVLS0?feature=share , https://www.youtube.com/watch?v=BX2uspkSxqM");
+};
+if (x === 30) {
+    document.write("Destynacja ważniejsza od życia? https://youtu.be/XeWYWU91imA");
+};
